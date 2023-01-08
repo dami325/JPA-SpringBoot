dbb18bfcc97f345e4b869a3baf21265721ab87a7
Judalm Park (dami) <wnekfa1004@naver.com>
2023-01-08 18:16:29 +0900

    상품 도메인 개발

--- src/test/java/jpabook/jpashop/domain/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/domain/service/MemberServiceTest.java
@@ -1,7 +1,8 @@
 package jpabook.jpashop.domain.service;
 
 import jpabook.jpashop.domain.Member;
-import jpabook.jpashop.domain.repository.MemberRepository;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.service.MemberService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +20,8 @@ import static org.junit.Assert.fail;
 @Transactional /**스프링 기본 설정 = @Rollback(true)*/
 public class MemberServiceTest {
 
-    @Autowired MemberService memberService;
+    @Autowired
+    MemberService memberService;
     @Autowired MemberRepository memberRepository;
     @Autowired
     EntityManager em;
